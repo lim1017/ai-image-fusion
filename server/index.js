2abcbd44c650001f15d7c72e43bad6b33d63e814
tom <lim1017@hotmail.com>
2023-04-25 00:15:26 -0400

    fixed server starting improperly

--- server/index.js
+++ server/index.js
@@ -1,33 +1,32 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./mongodb/connect.js";
 
+import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
-  res.send("Hello from DALL-E!");
+  res.status(200).json({
+    message: "Hello from DALL.E!",
+  });
 });
 
 const startServer = async () => {
   try {
-    await connectDB(process.env.MONGO_DB_URL);
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is listening on port ${process.env.PORT}`);
-    });
-  } catch (err) {
-    console.log(err, "error connecting to db");
+    connectDB(process.env.MONGO_DB_URL);
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
   }
 };
 
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,14 +6,14 @@ dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(config);
+const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-  res.send("Hello from DALL-E!");
+  res.status(200).json({ message: "Hello from DALL-E route!" });
 });
 
 export default router;
