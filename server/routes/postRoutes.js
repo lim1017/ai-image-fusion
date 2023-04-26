379661218edc5ce853d5778f3f432fdc39db5573
tom <lim1017@hotmail.com>
2023-04-26 10:39:53 -0400

    added post routes, create post and get all posts

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -14,4 +14,27 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+
+    const photoURL = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({ name, prompt, photoURL: photoURL.url });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: true, message: err });
+  }
+});
+
 export default router;
