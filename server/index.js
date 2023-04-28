a2ceeaac27b5afe80acb7bab6666a4db63042fbb
tom <lim1017@hotmail.com>
2023-04-27 21:41:03 -0400

    added security headers for mime error

--- server/index.js
+++ server/index.js
@@ -15,6 +15,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
+// Security headers
+app.use((req, res, next) => {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  next();
+});
+
 app.get("/", async (req, res) => {
   res.status(200).json({
     message: "Hello from DALL.E!",
