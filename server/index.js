41346bbef7f212e1a8c34ea14d799f210a5c2c10
tom <lim1017@hotmail.com>
2023-05-08 14:16:16 -0400

    cors fix

--- server/index.js
+++ server/index.js
@@ -33,7 +33,19 @@ app.use(
 
 dotenv.config();
 
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ai-image-tom.netlify.app/",
+      "http://ai-image-tom.netlify.app/",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
