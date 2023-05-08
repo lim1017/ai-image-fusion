61b91e1429597bab5cb85e1be2b100cf1a922061
tom <lim1017@hotmail.com>
2023-05-08 16:24:34 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -33,18 +33,18 @@ app.use(
 
 dotenv.config();
 
-// app.use(cors());
+app.use(cors());
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ai-image-tom.netlify.app/",
-      "http://ai-image-tom.netlify.app/",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://ai-image-tom.netlify.app/",
+//       "http://ai-image-tom.netlify.app/",
+//     ],
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json({ limit: "50mb" }));
 
