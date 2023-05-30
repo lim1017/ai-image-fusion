d829bc9b03db72799c1f8941ebc659c27b504d22
tom <lim1017@hotmail.com>
2023-05-29 21:54:10 -0400

    added auth0 provider

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+console.log(window.location.origin, "window origin");
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="ai-images.us.auth0.com"
+      clientId="jrZ5OOSmVdQiSKlUaEHY6Nen9h6Lj1xo"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
