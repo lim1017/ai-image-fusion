f9813cab861502dff6e27261680a41583d1797b9
tom <lim1017@hotmail.com>
2023-07-25 14:14:10 -0400

    login/out working properly need to create user on backend to add favourites

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import { useNavigate } from "react-router-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, CreatePost } from "./pages";
-import Header from "./components/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Auth0Provider } from "@auth0/auth0-react";
-import MyPostsAndFavourite from "./pages/MyPostsAndFavourite";
+
+import { Content } from "./Content";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,16 +30,10 @@ const App = () => {
           redirect_uri: window.location.origin,
         }}
         onRedirectCallback={handleRedirectCallback}
+        cacheLocation="localstorage"
       >
         <QueryClientProvider client={queryClient}>
-          <Header />
-          <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/create-post" element={<CreatePost />} />
-              <Route path="/my-posts" element={<MyPostsAndFavourite />} />
-            </Routes>
-          </main>
+          <Content />
         </QueryClientProvider>
       </Auth0Provider>
     </div>
