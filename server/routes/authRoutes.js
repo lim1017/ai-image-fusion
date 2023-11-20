ffe09886cb6fe510a837328b8e9ab79c208ba0dd
tom <lim1017@hotmail.com>
2023-11-20 00:07:00 -0500

    setting up auth post route for creating users in db

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
 
+import User from "../mongodb/models/user.js";
+
 dotenv.config();
 
 const router = express.Router();
@@ -8,6 +10,21 @@ const router = express.Router();
 router.get("/", (req, res) => {
   console.log("in authRoute post!!!!!!!!!!!");
   res.send("hello from auth route");
+});
+
+router.post("/", async (req, res) => {
+  const { username, email } = req.body;
+  console.log(email, "email");
+  console.log(username, "username");
+
+  const user = await User.findOne({ email });
+  console.log(user, "user");
+
+  if (user) {
+    res.send("user exists");
+  } else {
+    res.send("new user");
+  }
 });
 
 export default router;
