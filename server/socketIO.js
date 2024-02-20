68130a211e14b40fba98a515ad9b8291f1114f00
tom <lim1017@hotmail.com>
2024-02-19 23:17:51 -0500

    refactored connection

--- server/socketIO.js
+++ server/socketIO.js
@@ -4,13 +4,17 @@ import { Server as SocketIOServer } from "socket.io";
 export const initSocketIO = (server) => {
   const io = new SocketIOServer(server, {
     cors: {
-      origin: "*",
+      origin: ["http://localhost:5173", "https://ai-image-tom.netlify.app/"], // your frontend application's origin
       methods: ["GET", "POST"], // allowed HTTP methods
     },
   });
   io.on("connection", (socket) => {
-    console.log("a user connected");
-    // You can add more event listeners here
+    console.log(`a user connected ${socket.id}`);
+
+    socket.on("chat", (data) => {
+      console.log(data);
+      socket.broadcast.emit("chat_response", data);
+    });
   });
 
   return io;
--- vite-project/src/pages/WebSocketChat.tsx
+++ vite-project/src/pages/WebSocketChat.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import io from "socket.io-client";
 
 export default function WebSocketChat() {
-  const handleConnect = () => {
+  useEffect(() => {
     const socket = io(`${import.meta.env.VITE_API_URL}`);
-    socket.emit("hello", "world");
+    socket.on("chat_response", (data) => {
+      console.log(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  });
+
+  const socket = io(`${import.meta.env.VITE_API_URL}`);
+  const handleConnect = () => {
+    socket.emit("chat", "Hello from chat world");
   };
   return (
     <div>
