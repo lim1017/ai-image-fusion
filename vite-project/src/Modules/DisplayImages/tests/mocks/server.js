37f6fb74a34a55b4b8d4b8fcf5ecde4e42a18fe6
tom <lim1017@hotmail.com>
2024-04-18 17:09:34 -0400

    clean up msw code

--- vite-project/src/Modules/DisplayImages/tests/mocks/server.js
+++
@@ -1,5 +0,0 @@
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
-
-// Create a server instance using a set of request handlers
-export const server = setupServer(...handlers);
